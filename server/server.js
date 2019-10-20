f05bb11d417c2410eafc4904936df2bae52e02fb
Jose Miralles <jemiralleslopez@gmail.com>
2019-10-21 00:45:59 +0100

    Modify name port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const publicPath = path.join(__dirname, '..', 'public');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -13,7 +13,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log('Server is up!');
 });
 
